7a6ee25f8e2aed2393be7f72c99dc89dbc2b00c0
Johannes Mitlmeier <github@spam.slzm.de>
2011-06-11 22:20:22 +0200

    implemented basic game over handling, see #1

---
+++ src/jazzy/logic/GameOver.py
@@ -0,0 +1,45 @@
+'''
+Copyright (c) 2011 Johannes Mitlmeier
+
+This file is part of Jazzy.
+
+Jazzy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/agpl.html>.
+'''
+
+
+class GameOver(object):
+
+    def __init__(self, board):
+        self.board = board
+        
+    
+    def noLegalMove(self):
+        # make sure we know possible moves
+        self.board.game.parsePossibleMoves()
+        # count
+        return len(self.board.game.possibleMoves) == 0
+
+    def inCheck(self):
+        return self.board.isInCheck(self.board.getCurrentPlayer())
+    
+    def noPiecesLeft(self):
+        currPlayer = self.board.getCurrentPlayer()
+        pieces = self.board.findPlayersPieces(currPlayer)
+        return len(pieces) == 0
+
+    def notRequiredPiecesLeft(self, pieceSet):        
+        currPlayer = self.board.getCurrentPlayer()
+        pieces = self.board.findPlayersPieces(currPlayer)
+        return pieceSet in set(pieces)
+    
