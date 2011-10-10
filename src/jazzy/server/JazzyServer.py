ec31fdad26dd3f204fba8473680e351288efa25b
Johannes Mitlmeier <github@spam.slzm.de>
2011-10-10 11:49:56 +0200

    include python3 shebang, encoding definition

--- src/jazzy/server/JazzyServer.py
+++ src/jazzy/server/JazzyServer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 '''
 Copyright (c) 2011 Johannes Mitlmeier
 
@@ -16,7 +19,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/agpl.html>.
 '''
-
 
 
 import time
